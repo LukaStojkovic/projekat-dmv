e91d6f7693e771315b34f7a40054149b24bb35e5
Luka Stojkovic <lukastojkovic006.ls@gmail.com>
2025-07-12 20:29:30 +0200

    Edit DeviceForm to use select input instead of basic input

--- frontend/src/components/DeviceForm.jsx
+++ frontend/src/components/DeviceForm.jsx
@@ -1,8 +1,17 @@
 import { Input } from "./ui/input";
 import { DialogClose } from "@radix-ui/react-dialog";
 import { DialogFooter } from "@/components/ui/dialog";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { Button } from "./ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
 
 export default function DeviceForm({
   handleSubmitForm,
@@ -15,6 +24,7 @@ export default function DeviceForm({
     formState: { errors },
     handleSubmit,
     reset,
+    control,
   } = useForm({ defaultValues });
 
   async function submitForm(data) {
@@ -49,21 +59,28 @@ export default function DeviceForm({
         </div>
         <div className="grid gap-3">
           <label htmlFor="type">Type</label>
-          <Input
-            id="type"
+          <Controller
             name="type"
-            placeholder="Type..."
-            {...register("type", {
+            control={control}
+            rules={{
               required: "Type is required",
-              maxLength: {
-                value: 20,
-                message: "Type cannot contain more than 20 characters",
-              },
-              min: {
-                value: 3,
-                message: "Type must have at least 3 characters",
-              },
-            })}
+            }}
+            render={({ field }) => (
+              <Select onValueChange={field.onChange} value={field.value ?? ""}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select device type" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Devices</SelectLabel>
+                    <SelectItem value="Radar">Radar</SelectItem>
+                    <SelectItem value="Semafor">Semafor</SelectItem>
+                    <SelectItem value="Senzor">Senzor</SelectItem>
+                    <SelectItem value="Kamera">Kamera</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            )}
           />
           {errors.type && (
             <p className="text-red-500 text-xs mt-1">{errors.type.message}</p>
